717f0b6c46020df55a9e129f74fa3c6edca6ad21
dikjain <dikshitmahanot2005@gmail.com>
2025-01-14 18:48:51 +0530

    fix it

--- app/page.js
+++ app/page.js
@@ -3,14 +3,17 @@ import Hero from './_components/Hero'
 import ProductList from './_components/ProductList'
 import Spline from '@splinetool/react-spline/next';
 
- function Home() {
+function Home() {
   return (
     <div className='relative'>
-      <div className='absolute top-0 left-0 w-full h-full max-[800px]:top-[-800px] z-[-1] bg-secondary'>
-        <Spline 
-          // scene="https://prod.spline.design/ClhST-Dp6iO3aqOA/scene.splinecode" 
-          scene="https://prod.spline.design/WM3SVaZhwQ1fD35t/scene.splinecode " 
-        />
+      <div className='absolute top-0 left-0 w-full h-full  z-[-1] bg-secondary'>
+        {/* Use CSS media queries instead of JS for responsive behavior */}
+        <div className='hidden max-[800px]:block'>
+          <Spline scene="https://prod.spline.design/it8qTJbBFOISljMe/scene.splinecode" />
+        </div>
+        <div className='block max-[800px]:hidden'>
+          <Spline scene="https://prod.spline.design/WM3SVaZhwQ1fD35t/scene.splinecode" />
+        </div>
       </div>
       <Hero/>
 
